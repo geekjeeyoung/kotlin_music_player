6be90ddd503b9037963f6c09cad88c3044e8ee7f
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2021-02-01 10:52:55 +0900

    Update gradle

--- app/src/main/java/fun/chezcandy/timberxclone/ui/activities/base/PermissionsActivity.kt
+++ app/src/main/java/fun/chezcandy/timberxclone/ui/activities/base/PermissionsActivity.kt
@@ -1,12 +1,12 @@
+@file:Suppress("MemberVisibilityCanBePrivate")
+
 package `fun`.chezcandy.timberxclone.ui.activities.base
 
 import `fun`.chezcandy.timberxclone.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class PermissionsActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_permissions)
-    }
+abstract class PermissionsActivity : AppCompatActivity() {
+
+    protected val permissionsManager by inject
 }
